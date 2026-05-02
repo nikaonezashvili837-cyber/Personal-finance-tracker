47cc38ea9722e5284c9fc98b351bb37cc8ecd515
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-05-02 22:13:49 +0400

    implementing basic project structure

--- Personal-finance-tracker/Program.cs
+++ Personal-finance-tracker/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace PersonalFinanceTracker
+{
+    class Program
+    {
+        public static void Main()
+        {
+        
+        }
+    }
+}
