07f1d8ab4f7ccc867865274b5df487612f665a69
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-05-02 22:23:11 +0400

    Feat:building numbered menu

--- Personal-finance-tracker/Program.cs
+++ Personal-finance-tracker/Program.cs
@@ -5,7 +5,24 @@ namespace PersonalFinanceTracker
     {
         public static void Main()
         {
-        
+            try
+            {
+                Console.WriteLine("===== Personal Finance Tracker =====\n" +
+                "1. Add Transaction\n" +
+                "2. View All Transactions\n" +
+                "3. View Summary\n" +
+                "4. Filter Transactions\n" +
+                "5. Save Data\n" +
+                "6. Load Data\n" +
+                "7. Exit\n" +
+                "====================================\n" +
+                "Please choose an option (1-7):");
+                byte MenuOption = Convert.ToByte(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
