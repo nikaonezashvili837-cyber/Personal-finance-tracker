e8eb796aa7b5d8e0b6dd417ce6d15c05ea0dade4
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-05-12 21:54:37 +0400

    Refactor:Moving AddExpense method to the separate partial class

--- Personal-finance-tracker/Program.cs
+++ Personal-finance-tracker/Program.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Text.Json;
 namespace PersonalFinanceTracker
 {
-    class Program
+    partial class Program
     {
         public static void Main()
         {
@@ -43,59 +43,10 @@ namespace PersonalFinanceTracker
                 Console.WriteLine("Error");
             }
         }
-        public static void AddExpense()
-        {
-            string jsonFile = File.ReadAllText("expenses.json");
-            List<Transaction> transactions = new List<Transaction>();
-            Console.WriteLine("Enter amount");
-            float trasactionAmount = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter type of transaction");
-            string? transactionType = Console.ReadLine();
-            Console.WriteLine("Enter category of transaction");
-            string? transactionCategory = Console.ReadLine();
-            if (transactionType == null || transactionCategory == null)
-            {
-                Console.WriteLine("error");
-                return;
-            }
-            if (transactionType != "Expense" && transactionType != "Income")
-            {
-                Console.WriteLine("Enter valid transaction type");
-                return;
-            }
-            Transaction transactioneObject = new Transaction(trasactionAmount, transactionType, transactionCategory);
-            string json;
-            if (jsonFile == "")
-            {
-                transactions.Add(transactioneObject);
-                json = JsonSerializer.Serialize(transactions, new JsonSerializerOptions { WriteIndented = true });
-            }
-            else
-            {
-                List<Transaction>? transactionsData = JsonSerializer.Deserialize<List<Transaction>>(jsonFile);
-                transactionsData.Add(transactioneObject);
-                json = JsonSerializer.Serialize(transactionsData, new JsonSerializerOptions { WriteIndented = true });
-            }
-            File.WriteAllText("expenses.json", json);
-            return;
-        }
         public static void ViewTransactions()
         {
             string jsonContent = File.ReadAllText("expenses.json");
             Console.WriteLine(jsonContent);
-        }
-    }
-    class Transaction
-    {
-
-        public float Amount { get; set; }
-        public string Type { get; set; }
-        public string Category { get; set; }
-        public Transaction(float amount, string type, string category)
-        {
-            Amount = amount;
-            Type = type;
-            Category = category;
         }
     }
 }
